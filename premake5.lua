cfd5e5ad0deab0b14edb4a5e5b1224ecb956dad0
Murgn <68158297+Murgn@users.noreply.github.com>
2021-08-10 15:43:41 +0100

    Added Premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Lilith"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Lilith"
+	location "Lilith"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LI_PLATFORM_WINDOWS",
+			"LI_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations.Debug"
+		defines "LI_DEBUG"
+		symbols "On"
+
+	filter "configurations.Release"
+		defines "LI_RELEASE"
+		optimize "On"
+
+	filter "configurations.Dist"
+		defines "LI_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Lilith/vendor/spdlog/include",
+		"Lilith/src"
+	}
+
+	links
+	{
+		"Lilith"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"LI_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations.Debug"
+		defines "LI_DEBUG"
+		symbols "On"
+
+	filter "configurations.Release"
+		defines "LI_RELEASE"
+		optimize "On"
+
+	filter "configurations.Dist"
+		defines "LI_DIST"
+		optimize "On"
