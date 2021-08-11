14438cefe617c34f02cb8e0db59c09a298ab9b70
Murgn <68158297+Murgn@users.noreply.github.com>
2021-08-11 21:47:01 +0100

    ImGui Events

--- Lilith/src/Lilith/Events/KeyEvent.h
+++ Lilith/src/Lilith/Events/KeyEvent.h
@@ -53,4 +53,20 @@ namespace Lilith {
 		EVENT_CLASS_TYPE(KeyPressed)
 	};
 
+	class LILITH_API KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
+
 }
--- Lilith/src/Lilith/ImGui/ImGuiLayer.h
+++ Lilith/src/Lilith/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Lilith/Layer.h"
 
+#include "Lilith/Events/ApplicationEvent.h"
+#include "Lilith/Events/KeyEvent.h"
+#include "Lilith/Events/MouseEvent.h"
+
 namespace Lilith {
 
 	class LILITH_API ImGuiLayer : public Layer
@@ -14,7 +18,15 @@ namespace Lilith {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
-	
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
